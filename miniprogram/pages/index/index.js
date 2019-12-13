a31c0366e043b81dfd8c01f486de3c427fd7d0dd
xm661126 <xm632866568@163.com>
2019-12-13 11:38:26 +0800

    v1.5
    主页修改完毕，准备完成其他页面

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,10 +3,21 @@ const app = getApp()
 
 Page({
   data: {
- 
+
+  },
+  changeToWarehousing: function() {
+    wx.navigateTo({
+      url: '/pages/cloud/cloud',
+    })
   },
 
   onLoad: function() {
- 
-}
+    wx.setNavigationBarTitle({
+      title: '胶水管理系统',
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#1BC3B8',
+    })
+  }
 })
